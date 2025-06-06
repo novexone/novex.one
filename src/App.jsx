31e920436ead3d31f7e30968c2f43226aadb7dd6
novexone <dev@mrtx0.com>
2025-06-06 10:06:25 +0200

    feat: add footer with copyright and social links
    - Create Footer component with copyright notice
    - Add email icon linking to contact@novex.one
    - Add GitHub icon linking to github.com/novexone
    - Implement Roboto Mono typography for copyright text
    - Add subtle border and hover effects for icons

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 // Dark Theme
 const theme = createTheme({
@@ -20,6 +21,7 @@ const theme = createTheme({
       primary: "#ffffff",
       secondary: "#b3b3b3",
     },
+    divider: "#333333",
   },
   typography: {
     fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
@@ -43,6 +45,9 @@ function App() {
 
         {/* Main Content */}
         <MainContent />
+
+        {/* Footer */}
+        <Footer />
       </Box>
     </ThemeProvider>
   );
